780746929406526ef5926e957d6de63934e7d860
Mikel Calvo <MikelCalvo@users.noreply.github.com>
2017-03-24 06:07:33 +0100

    Added a border
    Added a 3px black border to the color circles in order to prevent them from mixing with the dialog background

--- color-picker/src/main/java/org/xdty/preference/colorpicker/ColorPickerSwatch.java
+++ color-picker/src/main/java/org/xdty/preference/colorpicker/ColorPickerSwatch.java
@@ -17,7 +17,8 @@
 package org.xdty.preference.colorpicker;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -58,9 +59,14 @@ public class ColorPickerSwatch extends FrameLayout implements View.OnClickListen
     }
 
     protected void setColor(int color) {
-        Drawable[] colorDrawable = new Drawable[]
-                {getContext().getResources().getDrawable(R.drawable.color_picker_swatch)};
-        mSwatchImage.setImageDrawable(new ColorStateDrawable(colorDrawable, color));
+        GradientDrawable colorDrawable;
+
+        colorDrawable = new GradientDrawable();
+        colorDrawable.setShape(GradientDrawable.OVAL);
+        colorDrawable.setColor(color);
+        colorDrawable.setStroke(3, Color.BLACK);
+
+        mSwatchImage.setImageDrawable(colorDrawable);
     }
 
     private void setChecked(boolean checked) {
